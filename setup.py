ff611ecd089157b1823a9753625a067d46de8de0
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2021-11-22 17:39:10 -0800

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(this_directory / 'requirements.in') as f:
 
 setuptools.setup(
     name='metlo',
-    version='0.0.11',
+    version='0.0.12',
     author='S2 Labs Inc.',
     author_email='akshay@metlo.com',
     description='Metlo\'s Python SDK',
